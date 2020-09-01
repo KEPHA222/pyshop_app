309e410e71706ab02c5753dcb2d285497f592305
kepha222 <kephamotari1@gmail.com>
2020-09-02 02:39:55 +0300

    first commit

---
+++ README..md
@@ -0,0 +1,13 @@
+## Pollster app <br>
+This is a Pyshop application using Django and db.sqlite3. <br>
+
+## Usage <br>
+Django and pip install <br>
+
+## Run <br>
+python manage.py runserver <br>
+
+## Visit http://localhost:8000 <br>
+
+## db.sqlite3 <br>
+Migrate all the records after creating a model.
---
+++ products/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Product, Offer
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Product, ProductAdmin)
